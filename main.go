8c7eac5897d6d4345a03ebd9cbe02eb618dab9bd
YU HAN GOH <gohyuhan123456@gmail.com>
2025-10-25 11:31:56 +0800

    re-commit for a missed changed

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"gitti/api"
+	"gitti/api/git"
 	"gitti/tui"
 	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -14,20 +16,32 @@ func main() {
 	if err != nil {
 		panic(fmt.Sprintf("Failed to get current working directory: %v", err))
 	}
-	gitWorkerDaemon := api.NewGitWorkerDaemon(repoPath)
 
-	gittiUiModel := tui.NewGittiModel(repoPath, gitWorkerDaemon)
+	// create the channel that will be the bring to emit update event back to main thread
+	updateChannel := make(chan string)
+
+	// various initialization
+	debounceMS := 500 * time.Millisecond
+	git.InitGitBranch(repoPath)
+	git.InitGitFile(repoPath)
+	git.GitCommitInit(repoPath, false)
+	api.InitGitDaemon(repoPath, debounceMS, updateChannel)
+
+	// start the Git Daemon
+	api.GITDAEMON.Start()
+
+	gittiUiModel := tui.NewGittiModel(repoPath)
 	gitti := tea.NewProgram(
-		gittiUiModel,
+		&gittiUiModel,
 		tea.WithAltScreen(), // ← enables full-screen TUI mode
 		tea.WithMouseCellMotion(),
 	)
 
-	tui.StartGitUpdateListener(gitti, gitWorkerDaemon)
+	tui.StartGitUpdateListener(gitti, updateChannel)
 
 	if _, err := gitti.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
-		gitWorkerDaemon.Stop()
+		api.GITDAEMON.Stop()
 		os.Exit(1)
 	}
 
